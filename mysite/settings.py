4a4fe1bd11cecba778bee55d11fd5dbc7755f9ec
jaxsen h <jaxsenhoneycutt@gmail.com>
2020-10-24 01:16:17 -0500

    change to a 'waveo' project

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'waveo.apps.WaveoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
