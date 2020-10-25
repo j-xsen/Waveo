57aff10c814361a7034118b8efd65ea396bc78cc
jaxsen h <jaxsenhoneycutt@gmail.com>
2020-10-25 06:53:20 -0500

    add static

--- static/waveo/js/js.js
+++ static/waveo/js/js.js
@@ -2,29 +2,41 @@ let on_time = 0;
 const max_time = 12;
 let playing = false;
 
-$(".note").click(function(){
-    if($(this).attr("play") === "0"){
-        $(this).attr("play",1);
-        $(this).children('i').removeClass("far").addClass("fas");
-    } else{
-        $(this).attr("play",0);
-        $(this).children('i').removeClass("fas").addClass("far");
+let notes = []
+
+$(document).ready(function(){
+    for(let i = 0; i < max_time; i++){
+        notes.push([]);
     }
-});
 
-$(".play-button").click(function(){
-    if($(this).attr("play") === "0"){
-        $(this).attr("play",1);
-        $(this).html('<i class="fas fa-pause"></i>');
+    $(".note").click(function(){
+        if($(this).attr("play") === "0"){
+            $(this).attr("play",1);
+            $(this).children('i').removeClass("far").addClass("fas");
 
-        playing=true;
-        Play();
-    } else {
-        $(this).attr('play',0);
-        $(this).html('<i class="fas fa-play"></i>');
+            notes[$(this).attr("time") - 1] += $(this).attr("note");
+        } else{
+            $(this).attr("play",0);
+            $(this).children('i').removeClass("fas").addClass("far");
 
-        playing=false;
-    }
+            notes[$(this).attr("time") - 1] = notes[$(this).attr("time") - 1].replace($(this).attr("note"), "");
+        }
+    });
+
+    $(".play-button").click(function(){
+        if($(this).attr("play") === "0"){
+            $(this).attr("play",1);
+            $(this).html('<i class="fas fa-pause"></i>');
+
+            playing=true;
+            Play();
+        } else {
+            $(this).attr('play',0);
+            $(this).html('<i class="fas fa-play"></i>');
+
+            playing=false;
+        }
+    });
 });
 
 function Play(){
